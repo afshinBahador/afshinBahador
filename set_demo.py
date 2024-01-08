223f72152868f776a2935a0a91ca2dbde0d6bb54
afshinBahador <afshinbahador78@gmail.com>
2024-01-07 20:06:34 -0800

    Initial commit for data type

---
+++ set_demo.py
@@ -0,0 +1,27 @@
+# set : unordered data type
+# you "CAN NOT ' define empty set using literals.
+emptySet =set()
+sampleSet ={1,'A','9',3.86,'Dayche'}
+print(sampleSet)
+for _ in range(10):
+    print(sampleSet)
+print('=' * 40 )
+odd = set(range(1 , 50 , 2))    # members : 25
+power = {1 , 4 , 9 , 16 , 25 , 36 , 49}    # members : 7
+print(len(odd))      # teadade ooozve
+print(odd.intersection(power))
+print(power.intersection(odd))
+print(power.difference(odd))
+print(power.union(odd))
+print(len(power.union(odd)))    # 28 
+
+
+
+
+
+
+
+
+
+
+
